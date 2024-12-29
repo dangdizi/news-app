73ec333af5a6525af3f9ffffffb0b0a453a54bdb
Dang Dizi <dangdizi.com@gmail.com>
2024-12-30 02:09:42 +0700

    update

--- src/components/header/Form.tsx
+++ src/components/header/Form.tsx
@@ -1,8 +1,29 @@
+"use client";
+
+import { useRef } from "react";
+
 export const Form = () => {
-    return (
-        <form className="shadow-lg w-3/5 h-10 flex items-center">
-            <input placeholder="Enter your keyword..." className="flex-1 max-w-full w-full h-11 shadow-none outline-none px-4 duration-150 focus:shadow-white" />
-            <button className="px-8 h-11 bg-blue-600 text-white duration-150 hover:bg-blue- rounded-e" type="submit">Search</button>
-        </form>
-    )
-}
+    const ref = useRef<HTMLFormElement>(null);
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault(); // chặn load lại trang
+
+        // lấy giá trị từ form khi submit
+        const params = new FormData(ref.current as HTMLFormElement);
+        console.log(params.get("keywords"));
+    }
+  return (
+    <form ref={ref} onSubmit={handleSubmit} className="shadow-lg w-3/5 h-10 flex items-center">
+          <input
+              name="keywords"
+        placeholder="Enter your keyword..."
+        className="flex-1 max-w-full w-full h-11 shadow-none outline-none px-4 duration-150 focus:shadow-white"
+      />
+      <button
+        className="px-8 h-11 bg-blue-600 text-white duration-150 hover:bg-blue- rounded-e"
+        type="submit"
+      >
+        Search
+      </button>
+    </form>
+  );
+};
