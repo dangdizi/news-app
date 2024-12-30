0f80f28a8c5d17a340b9c0409afdf0e4c0f3cdbb
Dang Dizi <dangdizi.com@gmail.com>
2024-12-30 17:49:49 +0700

    main

--- src/services/articleService.ts
+++ src/services/articleService.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function getArticles() {
   try {
-    const response = await axios.get("http://localhost:3000/api/articles");
+    const response = await axios.get("api/articles");
     return response.data;
   } catch (error) {
     console.error(error);
